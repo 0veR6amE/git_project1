5fef51600627cfb574fe6b93894feae459e10300
f <ff@ff.ff>
2022-12-17 19:33:45 +0300

    We need more prints

--- programm.py
+++ programm.py
@@ -1,6 +1,9 @@
 def main():
     print('My first git program')
     print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+
 
 if __name__ == '__main__':
     main()
